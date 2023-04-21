2104ec60a8454c2a954ba909fa282d8565f3f8f9
NEDGE3 <739516@student.dusd.net>
2023-04-21 09:56:23 -0700

    Completed Uni5 5 started on bonus features
    finished restart button and difficulty buttons, started on bonus features finished lives counter and started on volume slider

--- UserInterfaceEdgeP2/Assets/Scripts/GameManager.cs
+++ UserInterfaceEdgeP2/Assets/Scripts/GameManager.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
     private int score;
+    private int lives;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
     public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+
         isGameActive = true;
+
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        UpdateLives(3);
+        titleScreen.gameObject.SetActive(false);
     }
 
     IEnumerator SpawnTarget()
@@ -38,10 +54,21 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livestoChange)
+    {
+        lives += livestoChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
 
     public void RestartGame()
